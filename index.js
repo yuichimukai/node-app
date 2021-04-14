1fc2f96a5900530f3fa88df291686b0964d46dd1
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-04-14 15:18:21 +0900

    add function and roop function

--- index.js
+++ index.js
@@ -25,3 +25,53 @@ switch (version) {
 		console.log("しらないバージョンです");
 		break;
 }
+
+let total = 0;
+
+for (let i = 0; i < 10; i++) {
+	total += i + 1;
+}
+console.log(total);
+
+function sum(numbers) {
+	let total = 0;
+	for (let i = 0; i < numbers.length; i++) {
+		total += numbers[i];
+	}
+	return total;
+}
+
+console.log(sum([1, 2, 3, 4, 5]));
+
+const array = [1, 2, 3];
+
+array.forEach((currentValue) => {
+	console.log(currentValue);
+});
+
+function sum(numbers) {
+	let total = 0;
+	numbers.forEach((num) => {
+		total += num;
+	});
+	return total;
+}
+
+sum([1, 2, 3, 4, 5]);
+
+function isEven(num) {
+	return num % 2 === 0;
+}
+function isEvenInclueded(numbers) {
+	let isEvenInclueded = false;
+	for (let i = 0; i < numbers.length; i++) {
+		const num = numbers[i];
+		if (isEven(num)) {
+			isEvenInclueded = true;
+			break;
+		}
+	}
+	return isEvenInclueded;
+}
+const array = [1, 5, 10, 15, 20];
+console.log(isEvenInclueded(array));
