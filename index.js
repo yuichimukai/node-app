ae8679a107f60b4cd4edd83800d002aea8439994
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-04-18 10:41:14 +0900

    add set and map

--- index.js
+++ index.js
@@ -395,3 +395,84 @@ async function fetchAB() {
 fetchAB().then((results) => {
 	console.log(results);
 });
+
+async function fetchResources(resources) {
+	const results = [];
+	for (let i = 0; i < resources.length; i++) {
+		const resource = resources[i];
+		const response = await dummyFetch(resource);
+		results.push(response.body);
+	}
+	return results;
+}
+
+class ShoppingCart {
+	constructor() {
+		this.items = new Map();
+	}
+
+	//カートに商品を追加する
+	addItem(item) {
+		const count = this.items.get(item) ?? 0;
+		this.items.set(item, count + 1);
+	}
+
+	//カート内の合計金額を返す
+	getTotalPrice() {
+		return Array.from(this.items).reduce((total, [item, count]) => {
+			return total + item.price * count;
+		}, 0);
+	}
+
+	//カートの中身を文字列にして返す
+	toString() {
+		return Array.from(this.items)
+			.map(([item, count]) => {
+				return `${item.name}:${item.count}`;
+			})
+			.join(",");
+	}
+}
+
+const shoppingCart = new ShoppingCart();
+
+const shopItems = [
+	{ name: "みかん", price: 100 },
+	{ name: "りんご", price: 200 },
+];
+
+// カートに商品を追加
+shoppingCart.addItem(shopItems[0]);
+shoppingCart.addItem(shopItems[0]);
+shoppingCart.addItem(shopItems[1]);
+
+// 合計金額を表示する
+console.log(shoppingCart.getTotalPrice());
+// カートの中身を表示
+console.log(shoppingCart.toString());
+
+const listenersMap = new WeakMap();
+
+class EventEmitter {
+	addListener(listener) {
+		const listeners = listenersMap.get(this) ?? [];
+		const newListeners = listeners.concat(listener);
+
+		listenersMap.set(this, newListeners);
+	}
+}
+
+let eventEmitter = new EventEmitter();
+eventEmitter.addListener(() => {
+	console.log("イベントが発火しました");
+});
+
+eventEmitter = null;
+
+const set = new Set(["a", "b"]);
+const results = [];
+for (const value of set) {
+	results.push(value);
+}
+
+console.log(results);
